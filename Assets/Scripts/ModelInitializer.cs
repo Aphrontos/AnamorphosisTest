8609d075de9ac82141ba3c6a2f2a89488ceffebb
Adiáphoros <47263323+Aphrontos@users.noreply.github.com>
2021-08-24 15:53:23 +0800

    Add files via upload

---
+++ Assets/Scripts/ModelInitializer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModelInitializer : MonoBehaviour
+{
+    public GameObject model,
+                      modelSpawnPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        model.SetActive(true);
+        model.transform.localScale = new Vector3(1, 1, 1);
+        //model.transform.localEulerAngles = new Vector3(0, 180, 0);
+        Instantiate(model, modelSpawnPoint.transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Solver.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Solver : MonoBehaviour
+{
+    public GameObject modelSpawnPoint,
+                      player,
+                      winningPoint;
+
+    private float accuracy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkAngle();
+    }
+
+    public void checkAngle()
+    {
+        
+        RaycastHit[] hits;
+        hits = Physics.RaycastAll(player.transform.position, transform.forward, 100.0F);
+
+        for (int i = 0; i < hits.Length; i++)
+        {
+            RaycastHit hit = hits[i];
+
+            if (hit.collider.GetComponent<CapsuleCollider>() != null)
+            {
+                accuracy = Vector3.Distance(modelSpawnPoint.transform.position, hit.point);
+                //Debug.Log("Model spawn point is at "+ modelSpawnPoint.transform.position + "\nLooking at "+ hit.point + "\nAccuracy:" + accuracy);
+            }
+        }
+    }
+}
